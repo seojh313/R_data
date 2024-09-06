ab5d2eacf5da06ff37c6ebfd08d5b612f90778e3
seojh313 <127711608+seojh313@users.noreply.github.com>
2024-09-06 11:18:47 +0900

    Add files via upload

--- 0906__1.R
+++ 0906__1.R
@@ -47,9 +47,68 @@ mtx=cbind(mtx,y)
 colnames(mtx)[4]="수학"
 mtx
 
+
+
+
+
+
 # ====
 # 리스트(list)
 
-lst = list(성명="김철수",나이="11",성적=c(70,80))
-lst
+lst1 = list(성명="김철수",나이="11",성적=c(70,80))
+lst1
 
+lst1.gender=list(성별="성별없음")
+lst1 =append(lst,lst.gender,after=1)
+lst1
+
+
+lst2 = list(성명="deb",성별="여",나이=20,성적=c(99,98))
+
+# 두 리스트 merge
+listTotal=list(lst1,lst2)
+listTotal
+#deb 이름만 출력하고자 할때
+listTotal[[2]][1]
+listTotal[[2]][[1]]
+listTotal[[2]]$성명
+#deb에 대한 리스트 출력
+listTotal[2]
+listTotal[[2]]
+
+
+
+
+
+
+
+#========
+#데이터프레임 2차원 형태의 테이블 구조에 데이터 저장
+
+df = data.frame(성명=c("김철수","김장수"),성별=c("남","남"),나이=c(11,12),수학=c(93,23))
+df
+#데이터 프레임 구조
+str(df)
+#데이터 프레임에 열단위 데이터추가
+df= cbind(df,음악=c(30,34))
+df
+
+#데이터 프레임에 행단위로 데이터추가
+df=rbind(df, data.frame(성명="김가나다",성별="남",나이=21,수학=122,음악=50))
+df
+
+#ㄷ데이터프레임에 저장된 요소의 값을 출력          
+df[2,3]
+#행데이터
+df[1,]
+#열데이터
+df[,4]
+
+#1,3행 데이터
+df[-2,]
+
+df[,-3]
+
+#값 변경
+df[2,2]="여"
+df
