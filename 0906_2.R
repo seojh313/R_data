5f12afe4d3581ce763f594b222415e2bdb856084
seojh313 <127711608+seojh313@users.noreply.github.com>
2024-09-06 11:40:31 +0900

    Add files via upload

---
+++ 0906_2.R
@@ -0,0 +1,11 @@
+install.package("ggplot2")
+library(ggplot2)
+
+dept=c("영업1팀","영업2팀","영업3팀")
+sales=c(12,7,9)
+df1 = data.frame(부서=dept,매출=sales,분기="1분기")
+df1
+
+ggplot(data=df1,aes(x=부서,y=매출))+
+  geom_bar(stat="identity")+
+  labs(title="부서별 영업실적",x="부서명",y="매출(억원)")
